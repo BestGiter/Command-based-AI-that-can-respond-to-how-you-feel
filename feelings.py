9d8036b817733cc9a840feaca2d39e0d229d288f
Lily Bevan <lilybevan31@gmail.com>
2025-04-28 16:57:20 +0100

    its one line now
    infinte stonks, less memory usage fr

--- feelings.py
+++ feelings.py
@@ -1,15 +1,1 @@
-from textblob import TextBlob
-
-user_input = input("welcome to this python game\nhow are you\n")
-blob = TextBlob(user_input)
-
-# polarity: -1 (negative) to +1 (positive)
-# subjectivity: 0 (objective) to 1 (subjective)
-sentiment = blob.sentiment.polarity
-
-if sentiment > 0:
-    print("Ok thats good")
-elif sentiment < 0:
-    print("Hope you have a better day")
-else:
-    print("Its fine to just be ok")
+from textblob import TextBlob; print("Ok thats good" if (s := TextBlob(input("welcome to this python game\nhow are you\n")).sentiment.polarity) > 0 else "Hope you have a better day" if s < 0 else "Its fine to just be ok")
