b0c401508aedcd8334a2293e2b552683f90a475a
Lily Bevan <lilybevan31@gmail.com>
2025-04-28 14:54:51 +0100

    fixes #6
    adds intelligence

--- feelings.py
+++ feelings.py
@@ -1,34 +1,15 @@
-#My python game
-print("welcome to this python game\nhow are you")
-answer = input()
-possibilities = {
-    "good": 1,
-    "ok": 0,
-    "bad": -1,
-    "terrible": -1,
-    "amazing": 1,
-    "fine": 0,
-    "well": 1,
-}
-feeling = None
-negate = 0
-for items in answer.split(" "):
-    if items == "not":
-        negate = 1-negate
-    if items in possibilities:
-        feeling = (feeling+possibilities[items])/2 if feeling != None else possibilities[items]
-result = feeling if negate==0 else -feeling
-if feeling == None:
-    print("you didn't say one of the keywords:")
-    for keys in possibilities:
-        print(keys+",")
-    print("and not")
-else:
-    if feeling < 0:
-        print("Hope you have a better day")
-    if feeling == 0:
-        print("Its fine to just be ok")
-    if feeling > 0:
-        print("Ok thats good")
-        
+from textblob import TextBlob
 
+user_input = input("welcome to this python game\nhow are you\n")
+blob = TextBlob(user_input)
+
+# polarity: -1 (negative) to +1 (positive)
+# subjectivity: 0 (objective) to 1 (subjective)
+sentiment = blob.sentiment.polarity
+
+if sentiment > 0:
+    print("Ok thats good")
+elif sentiment < 0:
+    print("Hope you have a better day")
+else:
+    print("Its fine to just be ok")
